1d24bb36ae8b3975959e2c16134aa2ccf5dd23db
Joshua Wiegand <jwiegand@stansberryresearch.com>
2019-06-13 09:44:24 -0400

    added init for nvim

--- init-env.sh
+++ init-env.sh
@@ -4,4 +4,5 @@
 ln -sfv "$HOME"/dotfiles/.zshrc "$HOME"
 ln -sfv "$HOME"/dotfiles/.vimrc "$HOME"
 ln -sfv "$HOME"/dotfiles/.vim/ "$HOME"
-
+mkdir -p "$HOME"/.config/nvim
+ln -sfv "$HOME"/dotfiles/init.vim "$HOME"/.config/nvim
