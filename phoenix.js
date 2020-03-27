fe99adb321a069d43dabd3ecd7955403ca394676
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-03-27 18:34:49 -0400

    phoenix configuration refactor
    - implemented in OOP methodology
    - changed constants to simple data types
    -

--- phoenix.js
+++ phoenix.js
@@ -3,221 +3,269 @@ Phoenix.set({
     openAtLogin: true,
 });
 
-// constants
-const combos = {
-    two: ['control', 'shift'],
-    three: ['control', 'option', 'command']
-};
+// combinations
+const CONTROL_OPTION = ['control', 'option'];
+const CONTROL_OPTION_COMMAND = ['control', 'option', 'command'];
 
-const orientations = {
-    both: 'both',
-    horizontal: 'horizontal',
-    vertical: 'vertical',
-};
+// orientations
+const BOTH = 'both';
+const HORIZONTAL = 'horizontal';
+const VERTICAL = 'vertical';
 
-const directions = {
-    center: 'center',
-    full: 'full',
-    ne: 'ne',
-    nw: 'nw',
-    se: 'se',
-    sw: 'sw',
-};
+// directions
+const CENTER = 'center';
+const FULL = 'full';
+const NE = 'ne';
+const NW = 'nw';
+const SE = 'se';
+const SW = 'sw';
 
-const sizes = {
-    eighth: 1 / 8,
-    quarter: 1 / 4,
-    third: 1 / 3,
-    half: 1 / 2,
-};
+// sizes
+const EIGHTH = 1 / 8;
+const QUARTER = 1 / 4;
+const THIRD = 1 / 3;
+const HALF = 1 / 2;
 
-let cache = [];
-
-// prototypes
-Window.prototype.setWindowFrame = function (location) {
-    const parent = this.screen().flippedVisibleFrame();
-    const id = this.hash();
-    let sizeIndex = 0;
-    let newFrame = {
-        y: null,
-        x: null,
-        width: null,
-        height: null,
-    };
-
-    let lastLocation = cache.find((window) => {
-        return this.hash() === window.id;
-    });
-
-    if (lastLocation && ++lastLocation.sizeIndex < location.sizes.length) {
-        sizeIndex = lastLocation.sizeIndex;
+class MyWindow {
+    constructor(window) {
+        this.window = window;
+        this.frame = window.frame();
+        this.parent = window.screen().flippedVisibleFrame();
     }
 
-    let item = {
-        id: id,
-        sizeIndex: sizeIndex,
-        orientation: location.orientation,
-        point: location.point,
-    };
-
-    cache = cache.filter((window) => {
-        return window.id !== id;
-    });
-
-    cache.push(item);
-
-    const size = location.sizes[sizeIndex];
-
-    switch (location.orientation) {
-        case orientations.vertical:
-            newFrame.width = Math.floor(parent.width * size);
-            newFrame.height = parent.height;
-            break;
-        case orientations.horizontal:
-            newFrame.width = parent.width;
-            newFrame.height = Math.floor(parent.height * size);
-            break;
-        case orientations.both:
-            newFrame.width = Math.floor(parent.width * size);
-            newFrame.height = Math.floor(parent.height * size);
-            break;
-        default:
-            newFrame.width = parent.width;
-            newFrame.height = parent.height;
-            break;
+    /**
+     *  find the difference between the parent and the frame
+     */
+    difference() {
+        return {
+            x: this.parent.x - this.frame.x,
+            y: this.parent.y - this.frame.y,
+            width: this.parent.width - this.frame.width,
+            height: this.parent.height - this.frame.height,
+        };
     }
 
-    switch (location.point) {
-        case directions.nw:
-            newFrame.y = parent.y;
-            newFrame.x = parent.x;
-            break;
-        case directions.ne:
-            newFrame.y = parent.y;
-            newFrame.x = parent.width - newFrame.width;
-            break;
-        case directions.sw:
-            newFrame.y = parent.y + parent.height - newFrame.height;
-            newFrame.x = parent.x;
-            break;
-        case directions.se:
-            newFrame.y = parent.y + parent.height - newFrame.height;
-            newFrame.x = parent.width - newFrame.width;
-            break;
-        case directions.center:
-            newFrame.y = parent.y + Math.floor(parent.height * sizes.eighth);
-            newFrame.x = Math.floor(parent.width * sizes.eighth);
-            break;
-        default:
-            newFrame.y = parent.y;
-            newFrame.x = parent.x;
-            break;
+    /**
+     * fill the window to the desire
+     * @param {string} orientation
+     * @param {number} size
+     */
+    fill(orientation) {
+        switch (orientation) {
+            case VERTICAL:
+                this.frame.width = Math.floor(this.parent.width * HALF);
+                this.frame.height = this.parent.height;
+                break;
+            case HORIZONTAL:
+                this.frame.width = this.parent.width;
+                this.frame.height = Math.floor(this.parent.height * HALF);
+                break;
+            case BOTH:
+                this.frame.width = Math.floor(this.parent.width * HALF);
+                this.frame.height = Math.floor(this.parent.height * HALF);
+                break;
+            case CENTER:
+                this.frame.width = Math.floor(this.parent.width * QUARTER * 3);
+                this.frame.height = Math.floor(this.parent.height * QUARTER * 3);
+                break;
+            default:
+                this.frame.width = this.parent.width;
+                this.frame.height = this.parent.height;
+                break;
+        }
+        return this;
     }
 
-    this.setFrame(Object.assign(this.frame(), newFrame));
+    /**
+     * fit the parent's screen
+     */
+    fit() {
+        const difference = this.difference();
+        if (difference.width < 0 || difference.height < 0) {
+            this.maximize();
+        }
+        return this;
+    }
+
+    /**
+     * halve the width of the frame
+     */
+    halve() {
+        this.frame.width /= 2;
+        return this;
+    }
+
+    /**
+     * maximize to the parent's screen
+     */
+    maximize() {
+        this.frame.width = this.parent.width;
+        this.frame.height = this.parent.height;
+        return this;
+    }
+
+    /**
+     * resize SE-corner by factor
+     * @param {Object} factor
+     */
+    resize(factor) {
+        const difference = this.difference();
+        let delta;
+        if (factor.width) {
+            delta = Math.min(this.parent.width * factor.width, difference.x + difference.width);
+            this.frame.width += delta;
+        } else if (factor.height) {
+            delta = Math.min(
+                this.parent.height * factor.height,
+                difference.height - this.frame.y,
+            );
+            this.frame.height += delta;
+        }
+        return this;
+    }
+
+    /**
+     * Move to screen
+     * @param {string} screen
+     */
+    screen(screen) {
+        this.parent = screen.flippedVisibleFrame();
+        return this;
+    }
+
+    /**
+     * Set frame
+     */
+    set() {
+        this.window.setFrame(this.frame);
+        this.frame = this.window.frame();
+        return this;
+    }
+
+    /**
+     * Move to cardinal directions NW, NE, SE, SW or relative direction CENTRE
+     * @param {string} position
+     */
+    to(position) {
+        switch (position) {
+            case NW:
+                this.frame.y = this.parent.y;
+                this.frame.x = this.parent.x;
+                break;
+            case NE:
+                this.frame.y = this.parent.y;
+                this.frame.x = this.parent.width - this.frame.width;
+                break;
+            case SW:
+                this.frame.y = this.parent.y + this.parent.height - this.frame.height;
+                this.frame.x = this.parent.x;
+                break;
+            case SE:
+                this.frame.y = this.parent.y + this.parent.height - this.frame.height;
+                this.frame.x = this.parent.width - this.frame.width;
+                break;
+            case CENTER:
+                this.frame.y = this.parent.y + Math.floor(this.parent.height * EIGHTH);
+                this.frame.x = Math.floor(this.parent.width * EIGHTH);
+                break;
+            default:
+                this.frame.y = this.parent.y;
+                this.frame.x = this.parent.x;
+                break;
+        }
+        return this;
+    }
+}
+
+// Fill in screen
+Window.prototype.fill = function (orientation) {
+    this.chain().fill(orientation);
+    return this.chain();
+};
+
+// Resize by factor
+Window.prototype.resize = function (factor) {
+    this.chain().resize(factor);
+    return this.chain();
+};
+
+// set the frame
+Window.prototype.set = function () {
+    this.chain().set();
+    return this.chain();
+}
+
+// To direction in screen
+Window.prototype.to = function (position) {
+    this.chain().to(position);
+    return this.chain();
 };
 
 // bindings
-Key.on('left', combos.three, () => {
-    const window = Window.focused();
+Key.on('left', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.vertical,
-            point: directions.nw,
-            sizes: [sizes.half, sizes.third, sizes.third * 2]
-        });
+        window.fill(VERTICAL).to(NW).set();
     }
 });
 
-Key.on('right', combos.three, () => {
-    const window = Window.focused();
+Key.on('right', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.vertical,
-            point: directions.ne,
-            sizes: [sizes.half, sizes.third, sizes.third * 2]
-        });
+        window.fill(VERTICAL).to(NE).set();
     }
 });
 
-Key.on('up', combos.three, () => {
-    const window = Window.focused();
+Key.on('up', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.horizontal,
-            point: directions.nw,
-            sizes: [sizes.half, sizes.third, sizes.third * 2]
-        });
+        window.fill(HORIZONTAL).to(NW).set();
     }
 });
 
-Key.on('down', combos.three, () => {
-    const window = Window.focused();
+Key.on('down', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.horizontal,
-            point: directions.sw,
-            sizes: [sizes.half, sizes.third, sizes.third * 2]
-        });
+        window.fill(HORIZONTAL).to(SW).set();
     }
 });
 
-Key.on('i', combos.three, () => {
-    const window = Window.focused();
+Key.on('i', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.both,
-            point: directions.nw,
-            sizes: [sizes.half]
-        });
+        window.fill(BOTH).to(NW).set();
     }
 });
 
-Key.on('o', combos.three, () => {
-    const window = Window.focused();
+Key.on('o', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.both,
-            point: directions.ne,
-            sizes: [sizes.half]
-        });
+        window.fill(BOTH).to(NE).set();
     }
 });
 
-Key.on('k', combos.three, () => {
-    const window = Window.focused();
+Key.on('k', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.both,
-            point: directions.sw,
-            sizes: [sizes.half]
-        });
+        window.fill(BOTH).to(SW).set();
     }
 });
 
-Key.on('l', combos.three, () => {
-    const window = Window.focused();
+Key.on('l', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.both,
-            point: directions.se,
-            sizes: [sizes.half]
-        });
+        window.fill(BOTH).to(SE).set();
     }
 });
 
-Key.on('delete', combos.three, () => {
-    const window = Window.focused();
+Key.on('delete', CONTROL_OPTION_COMMAND, () => {
+    const window = new MyWindow(Window.focused());
     if (window) {
-        window.setWindowFrame({
-            orientation: orientations.both,
-            point: directions.center,
-            sizes: [sizes.quarter * 3]
-        });
+        window.fill(CENTER).to(CENTER).set();
     }
 });
 
-Key.on('return', combos.three, () => {
+Key.on('return', CONTROL_OPTION_COMMAND, () => {
     const window = Window.focused();
     if (window) {
         window.maximize();
